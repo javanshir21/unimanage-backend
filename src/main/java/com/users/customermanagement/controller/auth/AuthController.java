d6b6554cb4d20cfb7993c9be4402792384da30f4
Javanshir <rustamovjavanshir7@gmail.com>
2025-12-27 02:49:21 +0100

    Initial Spring Boot project setup

--- src/main/java/com/users/customermanagement/controller/auth/AuthController.java
+++ src/main/java/com/users/customermanagement/controller/auth/AuthController.java
@@ -1,4 +1,21 @@
 package com.users.customermanagement.controller.auth;
 
+import com.users.customermanagement.model.User;
+import com.users.customermanagement.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "http://localhost:5173")
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody User user) {
+        return ResponseEntity.ok(userService.createUser(user));
+    }
 }
